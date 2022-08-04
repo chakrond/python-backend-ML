cb6c260bdc55a0958de64ac10796c3ea5348d796
Chakron <godlooter@gmail.com>
2022-08-03 20:39:22 -0600

    first commit

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+import configparser
+from src.app import create_app
+
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
+
+if __name__ == "__main__":
+    app = create_app()
+    app.config['DEBUG'] = True
+    app.config['MONGO_URI'] = config['PROD']['DB_URI']
+
+    app.run()
