97cc46fcdbe27eb6e32021e9b30a6d5fd951363d
Chakron <godlooter@gmail.com>
2022-08-05 14:28:14 -0600

    1st deploy

--- src/db/tasks.py
+++ src/db/tasks.py
@@ -8,22 +8,24 @@ from bson.objectid import ObjectId
 from bson.errors import InvalidId
 from datetime import datetime
 
-def get_db():
+# def get_db():
 
-    db = getattr(g, "_database", None)
+#     db = getattr(g, "_database", None)
 
-    if db is None:
+#     if db is None:
 
-        db = g._database = PyMongo(current_app).db
+#         db = g._database = PyMongo(current_app).db
        
-    return db
+#     return db
 
-# LocalProxy
-db = LocalProxy(get_db)
+# # LocalProxy
+# db = LocalProxy(get_db)
+
+# DB Instance
+db = g._database = PyMongo(current_app).db
 
 
 # Database commands
-
 def add_task(task_id, note):
 
     task = { 
